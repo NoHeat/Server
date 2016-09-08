dede4cba30908a8bfd738b1ecd2d23ac92cf6ecd
r0ug3_Mary <mgros050@fiu.edu>
2016-09-08 15:48:43 -0400

    csv parser and indexer work

--- pycsv_withindexer.py
+++ pycsv_withindexer.py
@@ -6,18 +6,20 @@ import glob
 import sys
  
 db = sys.argv[1]
-print ('test1') 
+
 conn = sqlite3.connect(db)
 conn.text_factory = str  # allows utf-8 data to be stored
  
 c = conn.cursor()
-print ('test2') 
+
 # traverse the directory and process each .csv file
 for csvfile in glob.glob(os.path.join(sys.argv[2], "*.csv")):
-    print ('test3')
+#for csvfile in glob.glob("*.csv"):
+
     # remove the path and extension and use what's left as a table name
     tablename = os.path.splitext(os.path.basename(csvfile))[0]
-    print ('test4') 
+	
+	#open csvfile and run thru for loop
     with open(csvfile, "rb") as f:
         reader = csv.reader(f)
  
@@ -26,30 +28,31 @@ for csvfile in glob.glob(os.path.join(sys.argv[2], "*.csv")):
             if header:
                 # gather column names from the first row of the csv
                 header = False
- 
-                sql = "DROP TABLE IF EXISTS ?" % tablename
+				#do not add table if already exists
+                sql = "DROP TABLE IF EXISTS %s" % tablename
                 c.execute(sql)
-                sql = "CREATE TABLE ? (?, ?)" % (tablename,
-                          ", ".join([ "? text" % column for column in row ]))
+				#create table for data
+                sql = "CREATE TABLE %s (%s)" % (tablename, ", ".join([ '"%s text"' % column for column in row ]))
                 c.execute(sql)
- 
+				#insert csv data for each column in a row
                 for column in row:
-                    if column.lower().endswith("_id"):
-                        index = "?__?" % ( tablename, column )
-                        sql = "CREATE INDEX ? on ? (?)" % ( index, tablename, column )
-                        c.execute(sql)
- 
-                insertsql = "INSERT INTO ? VALUES (?)" % (tablename,
-                            ", ".join([ "?" for column in row ]))
- 
-                rowlen = len(row)
+                	insertsql = "INSERT INTO %s VALUES (%s)" % (tablename, ", ".join([ "?" for column in row ]))
+                	rowlen = len(row)
+
             else:
                 # skip lines that don't have the right number of columns
-                #if len(row) == rowlen:
-                #    c.execute(insertsql, row)
- 
-                conn.commit()
+                if len(row) == rowlen:
+                    c.execute(insertsql, row)
+	    
+		#index table for searching 
+	    #sql = ("CREATE INDEX idx1 ON codes ('upc text');")
+        #    c.execute(sql)
+		index = "%s__%s" % ( tablename, column )
+        sql = "CREATE INDEX %s on %s (%s)" % ( index, tablename, ", ".join([ '"%s text"' % column for column in row ]))
+        c.execute(sql)
+
     conn.commit()
 
 c.close()
 conn.close()
+
